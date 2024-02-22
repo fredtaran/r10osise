aca14dc4ef41d0307ebdeb583411c2569f2b29a2
Fred Taran <ftaran04@gmail.com>
2024-02-22 22:00:28 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-body-bg">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'body-bg': '#EFECEC',
+        'button-bg': '#FC6736',
+        'banner-bg': '#0C2D57',
+        'button-border': '#FFB0B0',
+      },
+    },
+    fontFamily: {
+      'Roboto': ['Roboto, sans-serif'],
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+  },
+  plugins: [],
+}
+
