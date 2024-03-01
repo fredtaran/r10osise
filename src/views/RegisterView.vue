13655fc23c5b4bb0a57e3ee6bfd60631c87bbfb8
Fred Taran <ftaran04@gmail.com>
2024-03-01 21:28:43 +0800

    Registration working

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, computed } from 'vue';
+import { ref, reactive, computed } from 'vue'
 import useVuelidate from '@vuelidate/core'
 import { required, minLength, sameAs, helpers } from '@vuelidate/validators'
 import axios from 'axios'
@@ -10,10 +10,13 @@ const formData = reactive({
     confirm_password: ""
 })
 
+const isUsernameUnique = ref(true) // Track the uniqueness of the username
+const usernameTouched = ref(false); // Track if username input has been touched
+
 const rules = computed(() => {
     return {
         username: {
-            required,
+            required: helpers.withMessage("This field is required.", required),
             minLength: helpers.withMessage("Username should be at least 10 characters long.", minLength(10)),
             async isUnique(username) {
                 if(!helpers.req(username)) {
@@ -23,11 +26,11 @@ const rules = computed(() => {
             }
         },
         password: {
-            required,
+            required: helpers.withMessage("This field is required.", required),
             minLength: helpers.withMessage("Password should be at least 6 characters long.", minLength(6))
         },
         confirm_password: {
-            required,
+            required: helpers.withMessage("This field is required.", required),
             sameAs: helpers.withMessage("Password do not match.", sameAs(formData.password))
         }
     }
@@ -38,14 +41,13 @@ const v$ = useVuelidate(rules, formData)
 const submitForm = async () => {
     const validationResult = await v$.value.$validate()
     if (validationResult) {
-        
+
     }
 }
 
-const checkUsername = (username) => {
-    let queryString = `username=${username}`
-
-    axios.get(`check_username?${queryString}`);
+const checkUsername = async () => {
+    const response = await axios.get(`check_username?username=${formData.username}`)
+    isUsernameUnique.value = response.data.exist // Update the reactive variable
 }
 
 </script>
@@ -65,16 +67,21 @@ const checkUsername = (username) => {
                                 <label for="username" class="block mb-2 text-sm font-medium text-gray-900 ">Username</label>
                                 <input type="text" id="username" autocomplete="off"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                                    v-model="formData.username" required>
-                                <span class="text-sm text-red-700 font-italic font-bold" v-for="error in v$.username.$errors" :key="error.uid">{{ error.$message }}</span>
+                                    v-model="formData.username" @input="usernameTouched = true">
+                                <span class="text-sm text-red-700 font-italic font-bold"
+                                    v-for="error in v$.username.$errors" :key="error.uid">{{ error.$message }}</span>
+                                <span v-if="isUsernameUnique && usernameTouched" class="text-sm text-red-700 font-italic font-bold">Username
+                                    is already being
+                                    used.</span>
                             </div>
 
                             <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
                                 <input type="password" id="password" autocomplete="off"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                                    v-model="formData.password" required>
-                                <span class="text-sm text-red-700 font-italic font-bold" v-for="error in v$.password.$errors" :key="error.uid">{{ error.$message }}</span>
+                                    v-model="formData.password">
+                                <span class="text-sm text-red-700 font-italic font-bold"
+                                    v-for="error in v$.password.$errors" :key="error.uid">{{ error.$message }}</span>
                             </div>
 
                             <div>
@@ -82,15 +89,16 @@ const checkUsername = (username) => {
                                     Password</label>
                                 <input type="password" id="confirm_password" autocomplete="off"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                                    v-model="formData.confirm_password" required>
-                                <span class="text-sm text-red-700 font-italic font-bold" v-for="error in v$.confirm_password.$errors" :key="error.uid">{{ error.$message
-                                }}</span>
+                                    v-model="formData.confirm_password">
+                                <span class="text-sm text-red-700 font-italic font-bold"
+                                    v-for="error in v$.confirm_password.$errors" :key="error.uid">{{ error.$message }}</span>
                             </div>
 
                             <button type="submit"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 
-                            <span>Already have an account? Login <RouterLink :to="{ 'name': 'login' }" class="underline text-blue-600 font-bold hover:text-red-600">here</RouterLink>.</span>
+                            <span>Already have an account? Login <RouterLink :to="{ 'name': 'login' }"
+                                    class="underline text-blue-600 font-bold hover:text-red-600">here</RouterLink>.</span>
                         </div>
                     </form>
                 </div>
