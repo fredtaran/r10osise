5d780dd6178cfde2275ea425b00a12e5c92dd21f
fredtaran <ftaran04@gmail.com>
2024-04-17 16:19:43 +0800

    User profile

--- src/components/PictureAndStatusComponent.vue
+++ src/components/PictureAndStatusComponent.vue
@@ -4,7 +4,15 @@ import { ref } from 'vue'
 </script>
 
 <template>
-    <div class="max-w-full p-6 border-2 border-gray-200 shadow rounded-md flex items-center justify-center">
-        <img src="/Taran.jpg" alt="user" class="h-40 border border-stone-900 rounded-full">
+    <div class="p-6 border-2 border-gray-200 shadow rounded-md flex flex-col">
+        <div class="flex justify-center items-center">
+            <img src="/Taran.jpg" alt="user" class="h-40 border border-stone-900 rounded-full">
+        </div>
+
+        <div class="mt-5">
+            <h3>Status: Active</h3>
+            <h4>Date of Registration: 12/31/2023</h4>
+            <h4>Date of Expiration: 12/31/2024</h4>
+        </div>
     </div>
 </template>
