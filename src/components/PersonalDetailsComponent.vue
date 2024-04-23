7d5cfc5f91c5349ec21237d89a3be12ed337b622
fredtaran <ftaran04@gmail.com>
2024-04-23 17:04:04 +0800

    Working with user dashboard

--- src/components/PersonalDetailsComponent.vue
+++ src/components/PersonalDetailsComponent.vue
@@ -3,20 +3,143 @@
 </script>
 
 <template>
-    <div class="min-h-[350px] p-6 border-2 border-gray-200 shadow rounded-md flex flex-col">
-        <div class="mb-2">
-            <h3 class="text-xl font-bold">Personal Information</h3>
-        </div>
+    <div class="min-h-[350px] p-6 border-2 border-gray-200 shadow rounded-md flex flex-row">
+        <!-- Left Panel -->
+        <div class="flex flex-col w-1/2 p-2">
+            <!-- Personal Info -->
+            <div class="mb-2">
+                <h3 class="text-xl font-bold">Personal Information</h3>
+            </div>
 
-        <div class="">
-            <table class="table-auto w-full">
-                <tbody>
-                    <tr>
-                        <td class="font-bold w-20">Name</td>
-                        <td>Taran, Fred Polinar</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="">
+                <table class="table-auto w-full">
+                    <tbody>
+                        <tr>
+                            <td class="w-40">Birthdate:</td>
+                            <td class="font-bold">10/21/1996</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="w-40">Place of Birth:</td>
+                            <td class="font-bold">Cagayan de Oro City</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-40">Civil Status:</td>
+                            <td class="font-bold">Single</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.Personal Info -->
+
+            <!-- Address -->
+            <div class="mt-3 mb-2">
+                <h3 class="text-xl font-bold">Address Details</h3>
+            </div>
+
+            <div class="">
+                <table class="table-auto w-full">
+                    <tbody>
+                        <tr>
+                            <td class="w-40">Permanent:</td>
+                            <td class="font-bold">Z-9, Anhawon, Bulua, Cagayan de Oro</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="w-40">Current:</td>
+                            <td class="font-bold">Z-9, Anhawon, Bulua, Cagayan de Oro</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.Address -->
+
+            <!-- Company Details -->
+            <div class="mt-3 mb-2">
+                <h3 class="text-xl font-bold">Company Details</h3>
+            </div>
+
+            <div class="">
+                <table class="table-auto w-full">
+                    <tbody>
+                        <tr>
+                            <td class="w-40">Company Name:</td>
+                            <td class="font-bold">Mines and Geosciences Bureau - X</td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="w-40">Company Address:</td>
+                            <td class="font-bold">MGB-X Bldg., DENR-X Compound, Puntod, Cagayan de Oro</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.Company Details -->
         </div>
+        <!-- /.Left Panel -->
+
+        <!-- Right Panel -->
+        <div class="flex flex-col w-1/2 p-2">
+            <!-- Nationality -->
+            <div class="mb-2">
+                <h3 class="text-xl font-bold">Nationality Details</h3>
+            </div>
+
+            <div class="">
+                <table class="table-auto w-full">
+                    <tbody>
+                        <tr>
+                            <td class="w-60">Nationality:</td>
+                            <td class="font-bold">Filipino</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Acquisition Method:</td>
+                            <td class="font-bold">by Birth</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Last Foreign Address:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Date of Arrival to Philippines:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Landing Certificate No.:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Contract of Employment:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Nature of Employment:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+
+                        <tr>
+                            <td class="w-60">Name of Company:</td>
+                            <td class="font-bold">N/A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.Nationality -->
+        </div>
+        <!-- /.Right Panel -->
     </div>
 </template>
+
+<style scoped>
+td {
+    padding-top: 3px;
+    padding-bottom: 3px;
+}
+</style>
--- src/components/PictureAndStatusComponent.vue
+++ src/components/PictureAndStatusComponent.vue
@@ -34,5 +34,11 @@ import { ref } from 'vue'
                 </tbody>
             </table>
         </div>
+
+        <div class="flex flex-row justify-center items-center mt-3">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Apply for Permit</button>
+            <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Renew Permit</button>
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Cancel Permit</button>
+        </div>
     </div>
 </template>
